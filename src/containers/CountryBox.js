cc8e1205c5cb942475fabfa5138f512012a7462a
Laura McGarvey <lauraamcg@gmail.com>
2023-01-21 11:00:42 +0000

    connected the API to the app

--- src/containers/CountryBox.js
+++ src/containers/CountryBox.js
@@ -4,7 +4,17 @@ import Header from "../components/Header";
 
 const CountryBox = () => {
 
-    
+    const [countries, setCountries] = useState([])
+
+    useEffect(() => {
+        getCountries()
+    })
+
+    const getCountries = function () {
+        fetch (`https://restcountries.com/v3.1/all`)
+        .then (res => res.json())
+        .then (countries => setCountries(countries))
+    }
 
     return (
         <div>
